8e5e8aa63f8603f4230c319cdbbe0b163bb6da6f
Andreas Heix <andreas.heix@sap.com>
2023-02-15 09:37:26 -0800

    Still not pretty

--- distance_hbw.py
+++ distance_hbw.py
@@ -3,6 +3,7 @@
 import requests
 import os
 import csv
+import datetime
 
 class StartAndDestination:
     def __init__(self, start_city, destination_city):
@@ -25,25 +26,25 @@ class Voyage:
     def __init__(self, start_city, destination_city, distance, duration):
         self.start_city = start_city.strip()
         self.destination_city = destination_city.strip()
-        self.distance = distance.strip()
-        self.duration = duration.strip()
+        self.distance = distance
+        self.duration = duration
 
     def from_array(entries):
         return Voyage(entries[0], entries[1], entries[2], entries[3])
 
     def get_csv_entry(self):
-        return f"{self.start_city}, {self.destination_city}, {self.distance}, {self.duration}\n"
+        return f"{self.start_city}, {self.destination_city}, {self.distance}, {self.duration}"
 
     def __str__(self):
         return f"{self.start_city}, {self.destination_city}, {self.distance}, {self.duration}"
 
-def read_from_google(start_and_destination):
-    return Voyage(start_and_destination.start_city, start_and_destination.destination_city, "98", "4551")
-
 def write_entries_to_file_cache(new_file_entries_to_write):
+
+    now = f", {datetime.datetime.utcnow()}"
+
     lines_to_write = []
     for entry in new_file_entries_to_write:
-        lines_to_write.append(entry.get_csv_entry())
+        lines_to_write.append(entry.get_csv_entry() + now + "\n")
 
     print(lines_to_write)    
 
@@ -51,8 +52,10 @@ def write_entries_to_file_cache(new_file_entries_to_write):
         cache_file.writelines(lines_to_write)
         
 def calculate_distance(origin, destination):
+    #for testing witout paying:
+    #return Voyage(origin, destination, "98", "4551")
     API_KEY = os.getenv("API_KEY")
-    print(API_KEY)
+    #print(API_KEY)
     '''
     we expect:
     {'destination_addresses': ['Bietigheim-Bissingen, Germany'], 
@@ -67,44 +70,53 @@ def calculate_distance(origin, destination):
     response = requests.get(url)
     result = response.json()
     print(result)
-    return result['rows'][0]['elements'][0]['distance']['value']
+    
+    distance = result['rows'][0]['elements'][0]['distance']['value']
+    duration = result['rows'][0]['elements'][0]['duration']['value']
+    return Voyage(origin, destination, distance, duration)
+    #return result['rows'][0]['elements'][0]['distance']['value']
 
-def generate_html_table(city_list):
-    city_distances = {}
-    for start_city in city_list:
-        city_distances[start_city] = {}
-        for target_city in city_list:
-            if start_city == target_city:
-                city_distances[start_city][target_city] = 0
-            else:
-                city_distances[start_city][target_city] = calculate_distance(start_city, target_city)
+def generate_html_table(city_list, cache_entries, field_selector = "distance"):
+    #city_distances = {}
+    # for start_city in city_list:
+    #     city_distances[start_city] = {}
+    #     for target_city in city_list:
+    #         if start_city == target_city:
+    #             city_distances[start_city][target_city] = 0
+    #         else:
 
-    table = "<table>"
+    #             city_distances[start_city][target_city] = calculate_distance(start_city, target_city)
+
+    dummy_voyage = Voyage("same_city", "same_city", "0", "0")
+
+    table = "<table>\n"
     table += "<tr>"
     table += "<th>City</th>"
-    for start_city in city_distances:
+    for start_city in city_list:
         table += "<th>{}</th>".format(start_city)
     table += "</tr>"
 
-    for start_city in city_distances:
+    for start_city in city_list:
         table += "<tr>"
         table += "<td>{}</td>".format(start_city)
-        for target_city in city_distances:
-            distance = city_distances[start_city][target_city]
-            table += "<td>{}</td>".format(distance)
+        for target_city in city_list:
+            start_and_destination = StartAndDestination(start_city, target_city)
+
+            cache_entry = cache_entries.get(start_and_destination, dummy_voyage)
+            value = "0"
+            if field_selector == "distance":
+                value = f"{int(float(cache_entry.distance) / 1000)} km"
+            else:
+                value = datetime.timedelta(seconds=int(cache_entry.duration))
+
+            table += "<td>{}</td>".format(value)
         table += "</tr>"
+        table += "\n"
 
     table += "</table>"
     return table
 
 # Example usage
-s_u_d_1 = StartAndDestination("wurst", "brot")
-s_u_d_2 = StartAndDestination("wurst", "brot")
-if s_u_d_1 == s_u_d_2:
-    print("it works")
-else:
-    print("it does not")    
-
 city_list = ["Aalen", "Bietigheim", "Bruchsal", "Karlsruhe", "Lahr"]
 cache_file_name = "distances.csv"
 cache_entries={}
@@ -126,12 +138,18 @@ for start_city in city_list:
             continue
         if not cache_entries.get(start_and_destination, None):
             print(f"Need to retrieve distance for {start_and_destination} from google...")
-            voyage = read_from_google(start_and_destination)
+            voyage = calculate_distance(start_city, destination_city)
             new_file_entries_to_write.append(voyage)
+            cache_entries[start_and_destination]=voyage
 
 write_entries_to_file_cache(new_file_entries_to_write)
 
-exit()        
-read_from_cache_or_api(city_list)
-html_table = generate_html_table(city_list)
+html_table = generate_html_table(city_list, cache_entries)
 print(html_table)
+with open("distances.html", "w") as distance_file:
+    distance_file.write(html_table)
+
+html_table = generate_html_table(city_list, cache_entries, "duration")
+print(html_table)
+with open("duration.html", "w") as duration_file:
+    duration_file.write(html_table)    
